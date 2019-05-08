925544db3f29bc867aa634a47e0ccde5e64949e4
TheMagzuz <markus.dam123@gmail.com>
2019-05-08 12:12:05 +0200

    Add a few more utility functions for tokens.js

--- tokens.js
+++ tokens.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 const privateKey = fs.readFileSync(__dirname + '/private.key');
+const publicKey = fs.readFileSync(__dirname + '/publickey');
 
 var exports = module.exports = {}
 
@@ -10,4 +11,9 @@ exports.getToken = (username, expiresIn) => {
     return jwt.sign({username: username}, privateKey, {expiresIn: expiresIn});
 }
 
+exports.verify = (token) => {
+    return jwt.verify(token, privateKey);
+}
+
 exports.privateKey = privateKey;
+exports.publicKey = publicKey;
