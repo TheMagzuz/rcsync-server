ac830510d0e147ca0f7268aa1516e02ea3f30a70
ThMagzuz <markus.dam123@gmail.com>
2019-07-06 13:09:07 +0000

    Fix a stray route that was causing problems

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-const tokens = require('../tokens')
-
-router.get('/rc/:user/:rc', function(req, res, next) {
-    var decoded;
-    try {
-        console.log(req.user);
-    } catch (e) {
-        res.status(500).end();
-    }
-});
-
 module.exports = router;
